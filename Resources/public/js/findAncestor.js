55bc16baf767cfe651781ce01aff4b07c1ca029d
MesClics <mesclics@hotmail.com>
2020-01-27 10:26:00 +0100

    FEATURE: findNearest function added

--- Resources/public/js/findAncestor.js
+++ Resources/public/js/findAncestor.js
@@ -4,3 +4,8 @@ function findAncestor(el, cls) {
     while ((el = el.parentElement) && !el.classList.contains(cls));
     return el;
 }
+
+function findNearest(el, cls) {
+    while ((el = el.parentElement) && !el.querySelector(cls));
+    return el;
+}
