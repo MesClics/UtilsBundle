dda321e4e685de9c6a19663d51b38deb42b077bf
MesClics <mesclics@hotmail.com>
2020-02-07 19:13:45 +0100

    FEATURE: now we can add class to widget within the controllers

--- Widget/Widget.php
+++ Widget/Widget.php
@@ -5,6 +5,7 @@ namespace MesClics\UtilsBundle\Widget;
 use Symfony\Component\HttpFoundation\Request;
 abstract class Widget{
     protected $handler;
+    protected $class;
 
     protected function createForm($type, $data = null, $options = array()){
         return $this->handler->form_factory->create($type, $data, $options);
@@ -18,6 +19,20 @@ abstract class Widget{
         if($widget->getHandler()){
             $widget->getHandler()->handleRequest($this, $request);
         }
+    }
+
+    public function addClass(string $class){
+        $toArray = explode(" ", $this->class);
+        $toArray[] = $class;
+        $this->class = implode(" ", $toArray);
+    }
+
+    public function setClass(string $class){
+        $this->class = $class;
+    }
+
+    public function getClass(){
+        return $this->class;
     }
     
     abstract public function getName();
